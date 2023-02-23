b53a27e3b29040f39d7db6cf51fb0dfed96c87e3
Davide QUARANTIELLO <davide.quarantiello@st.com>
2023-02-23 09:15:26 +0100

    Add VL53L7CX driver and relative examples

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # VL53L7CX
-Arduino library to support the VL53L7CX Time-of-Flight 8x8 multizone ranging sensor with 90 degrees FoV
+Arduino library to support the VL53L7CX Time-of-Flight 8x8 multizone ranging sensor with wide field view.
+
+## API
+
+This sensor uses I2C to communicate. And I2C instance is required to access to the sensor.
+The APIs provide simple distance measure and multizone detection in both polling and interrupt modes.
+
+## Examples
+
+The examples contained in this library are based on VL53L7CX-SATEL sensor board. You need to connect the VL53L7CX-SATEL sensor board directly to the Nucleo board with wires as explained below:
+- pin 1 (GND) of the VL53L7CX satellite connected to GND of the Nucleo board
+- pin 2 (IOVDD) of the VL53L7CX satellite connected to 3V3 pin of the Nucleo board
+- pin 3 (AVDD) of the VL53L7CX satellite connected to 5V pin of the Nucleo board
+- pin 4 (PWREN) of the VL53L7CX satellite connected to pin A5 of the Nucleo board
+- pin 5 (LPn) of the VL53L7CX satellite connected to pin A3 of the Nucleo board
+- pin 6 (SCL) of the VL53L7CX satellite connected to pin D15 (SCL) of the Nucleo board
+- pin 7 (SDA) of the VL53L7CX satellite connected to pin D14 (SDA) of the Nucleo board
+- pin 8 (I2C_RST) of the VL53L7CX satellite connected to pin A1 of the Nucleo board
+- pin 9 (INT) of the VL53L7CX satellite connected to pin A2 of the Nucleo board 
+
+There are 2 examples with the VL53L7CX library:
+
+* VL53L7CX_HelloWorld: This example code is to show how to get multizone detection and proximity values of the VL53L7CX satellite sensor in polling mode.
+
+* VL53L7CX_ThresholdsDetection: This example code is to show how to configure the thresholds detection of the VL53L7CX satellite sensor.
+
+## Documentation
+
+You can find the source files at
+https://github.com/stm32duino/VL53L7CX
+
+The VL53L7CX datasheet is available at
+https://www.st.com/en/imaging-and-photonics-solutions/VL53L7CX.html
---
+++ examples/VL53L7CX_HelloWorld/VL53L7CX_HelloWorld.ino
@@ -0,0 +1,299 @@
+ /**
+ ******************************************************************************
+ * @file    VL53L7CX_HelloWorld.ino
+ * @author  STMicroelectronics
+ * @version V1.0.0
+ * @date    16 January 2023
+ * @brief   Arduino test application for STMicroelectronics VL53L7CX
+ *          proximity sensor satellite based on FlightSense.
+ *          This application makes use of C++ classes obtained from the C
+ *          components' drivers.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT(c) 2021 STMicroelectronics</center></h2>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
+/*
+ * To use these examples you need to connect the VL53L7CX satellite sensor directly to the Nucleo board with wires as explained below:
+ * pin 1 (GND) of the VL53L7CX satellite connected to GND of the Nucleo board
+ * pin 2 (IOVDD) of the VL53L7CX satellite connected to 3V3 pin of the Nucleo board
+ * pin 3 (AVDD) of the VL53L7CX satellite connected to 5V pin of the Nucleo board
+ * pin 4 (PWREN) of the VL53L7CX satellite connected to pin A5 of the Nucleo board
+ * pin 5 (LPn) of the VL53L7CX satellite connected to pin A3 of the Nucleo board
+ * pin 6 (SCL) of the VL53L7CX satellite connected to pin D15 (SCL) of the Nucleo board
+ * pin 7 (SDA) of the VL53L7CX satellite connected to pin D14 (SDA) of the Nucleo board
+ * pin 8 (I2C_RST) of the VL53L7CX satellite connected to pin A1 of the Nucleo board
+ * pin 9 (INT) of the VL53L7CX satellite connected to pin A2 of the Nucleo board
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <Arduino.h>
+#include <Wire.h>
+#include <vl53l7cx_class.h>
+
+
+#ifdef ARDUINO_SAM_DUE
+  #define DEV_I2C Wire1
+#else
+  #define DEV_I2C Wire
+#endif
+#define SerialPort Serial
+
+#define LPN_PIN A3
+#define I2C_RST_PIN A1
+#define PWREN_PIN A5
+
+void print_result(VL53L7CX_ResultsData *Result);
+void clear_screen(void);
+void handle_cmd(uint8_t cmd);
+void display_commands_banner(void);
+
+// Components.
+VL53L7CX sensor_vl53l7cx_top(&DEV_I2C, LPN_PIN, I2C_RST_PIN);
+
+bool EnableAmbient = false;
+bool EnableSignal = false;
+uint8_t res = VL53L7CX_RESOLUTION_4X4;
+char report[256];
+
+/* Setup ---------------------------------------------------------------------*/
+void setup()
+{
+
+  // Enable PWREN pin if present
+  if (PWREN_PIN >= 0) {
+    pinMode(PWREN_PIN, OUTPUT);
+    digitalWrite(PWREN_PIN, HIGH);
+    delay(10);
+  }
+
+  // Initialize serial for output.
+  SerialPort.begin(460800);
+
+  // Initialize I2C bus.
+  DEV_I2C.begin();
+
+  // Configure VL53L7CX component.
+  sensor_vl53l7cx_top.begin();
+
+  sensor_vl53l7cx_top.init_sensor();
+
+  // Start Measurements
+  sensor_vl53l7cx_top.vl53l7cx_start_ranging();
+}
+
+void loop()
+{
+  VL53L7CX_ResultsData Results;
+  uint8_t NewDataReady = 0;
+  uint8_t status;
+
+  do {
+    status = sensor_vl53l7cx_top.vl53l7cx_check_data_ready(&NewDataReady);
+  } while (!NewDataReady);
+
+  if ((!status) && (NewDataReady != 0)) {
+    status = sensor_vl53l7cx_top.vl53l7cx_get_ranging_data(&Results);
+    print_result(&Results);
+  }
+
+  if (Serial.available()>0)
+  {
+    handle_cmd(Serial.read());
+  }
+  delay(1000);
+}
+
+void print_result(VL53L7CX_ResultsData *Result)
+{
+  int8_t i, j, k, l;
+  uint8_t zones_per_line;
+  uint8_t number_of_zones = res;
+
+  zones_per_line = (number_of_zones == 16) ? 4 : 8;
+
+  display_commands_banner();
+
+  SerialPort.print("Cell Format :\n\n");
+  
+  for (l = 0; l < VL53L7CX_NB_TARGET_PER_ZONE; l++)
+  {
+    snprintf(report, sizeof(report)," \033[38;5;10m%20s\033[0m : %20s\n", "Distance [mm]", "Status");
+    SerialPort.print(report);
+
+    if(EnableAmbient || EnableSignal)
+    {
+      snprintf(report, sizeof(report)," %20s : %20s\n", "Signal [kcps/spad]", "Ambient [kcps/spad]");
+      SerialPort.print(report);
+    }
+  }
+
+  SerialPort.print("\n\n");
+
+  for (j = 0; j < number_of_zones; j += zones_per_line)
+  {
+    for (i = 0; i < zones_per_line; i++) 
+      SerialPort.print(" -----------------");
+    SerialPort.print("\n");
+    
+    for (i = 0; i < zones_per_line; i++)
+      SerialPort.print("|                 ");
+    SerialPort.print("|\n");
+  
+    for (l = 0; l < VL53L7CX_NB_TARGET_PER_ZONE; l++)
+    {
+      // Print distance and status 
+      for (k = (zones_per_line - 1); k >= 0; k--)
+      {
+        if (Result->nb_target_detected[j+k]>0)
+        {
+          snprintf(report, sizeof(report),"| \033[38;5;10m%5ld\033[0m  :  %5ld ",
+              (long)Result->distance_mm[(VL53L7CX_NB_TARGET_PER_ZONE * (j+k)) + l],
+              (long)Result->target_status[(VL53L7CX_NB_TARGET_PER_ZONE * (j+k)) + l]);
+              SerialPort.print(report);
+        }
+        else
+        {
+          snprintf(report, sizeof(report),"| %5s  :  %5s ", "X", "X");
+          SerialPort.print(report);
+        }
+      }
+      SerialPort.print("|\n");
+
+      if (EnableAmbient || EnableSignal )
+      {
+        // Print Signal and Ambient 
+        for (k = (zones_per_line - 1); k >= 0; k--)
+        {
+          if (Result->nb_target_detected[j+k]>0)
+          {
+            if (EnableSignal)
+            {
+              snprintf(report, sizeof(report),"| %5ld  :  ", (long)Result->signal_per_spad[(VL53L7CX_NB_TARGET_PER_ZONE * (j+k)) + l]);
+              SerialPort.print(report);
+            }
+            else
+            {
+              snprintf(report, sizeof(report),"| %5s  :  ", "X");
+              SerialPort.print(report);
+            }
+            if (EnableAmbient)
+            {
+              snprintf(report, sizeof(report),"%5ld ", (long)Result->ambient_per_spad[j+k]);
+              SerialPort.print(report);
+            }
+            else
+            {
+              snprintf(report, sizeof(report),"%5s ", "X");
+              SerialPort.print(report);
+            }
+          }
+          else
+          {
+            snprintf(report, sizeof(report),"| %5s  :  %5s ", "X", "X");
+            SerialPort.print(report);
+          }
+        }
+        SerialPort.print("|\n");
+      }
+    }
+  }
+  for (i = 0; i < zones_per_line; i++)
+   SerialPort.print(" -----------------");
+  SerialPort.print("\n");
+}
+
+void toggle_resolution(void)
+{
+  sensor_vl53l7cx_top.vl53l7cx_stop_ranging();
+
+  switch (res)
+  {
+    case VL53L7CX_RESOLUTION_4X4:
+      res = VL53L7CX_RESOLUTION_8X8;
+      break;
+
+    case VL53L7CX_RESOLUTION_8X8:
+      res = VL53L7CX_RESOLUTION_4X4;
+      break;
+
+    default:
+      break;
+  }
+  sensor_vl53l7cx_top.vl53l7cx_set_resolution(res);
+  sensor_vl53l7cx_top.vl53l7cx_start_ranging();
+}
+
+void toggle_signal_and_ambient(void)
+{
+  EnableAmbient = (EnableAmbient) ? false : true;
+  EnableSignal = (EnableSignal) ? false : true;
+}
+
+void clear_screen(void)
+{
+  snprintf(report, sizeof(report),"%c[2J", 27); /* 27 is ESC command */
+  SerialPort.print(report);
+}
+
+void display_commands_banner(void)
+{
+  snprintf(report, sizeof(report),"%c[2H", 27); /* 27 is ESC command */
+  SerialPort.print(report);
+
+  Serial.print("53L7A1 Simple Ranging demo application\n");
+  Serial.print("--------------------------------------\n\n");
+
+  Serial.print("Use the following keys to control application\n");
+  Serial.print(" 'r' : change resolution\n");
+  Serial.print(" 's' : enable signal and ambient\n");
+  Serial.print(" 'c' : clear screen\n");
+  Serial.print("\n");
+}
+
+void handle_cmd(uint8_t cmd)
+{
+  switch (cmd)
+  {
+    case 'r':
+      toggle_resolution();
+      clear_screen();
+      break;
+
+    case 's':
+      toggle_signal_and_ambient();
+      clear_screen();
+      break;
+
+    case 'c':
+      clear_screen();
+      break;
+
+    default:
+      break;
+  }
+}
